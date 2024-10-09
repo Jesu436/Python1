a03665bf4134b80108e700c5f834431e113cff8e
Jesus Duran <jesusduran141014@gmail.com>
2024-10-09 13:23:32 +0200

    Calculadora

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-print("Hello World"); 
-
+first_number = int(input('Type the first number: ')) ;\
+second_number = int(input('Type the second number: ')) ;\
+print("The sum is: ", first_number + second_number)
