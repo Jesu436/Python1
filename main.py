ac03c9a8f71dc221f8911a9d2662b62adaa06c76
Jesus Duran <jesusduran141014@gmail.com>
2024-10-14 11:42:47 +0200

    except con if,elif

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-def main():
-    try:
-        configuration = open ( 'config.txt')
-    except FileNotFoundError:
-        print("Couldn`t fint the config.txt file!")
-    except IsADirectoryError:
-        print("Found config.txt but it is a directory, couldn`t read it")
-    except Exception as e:
-        print(type(e))
-    except PermissionError as e:
-        print(e)
-
-main()
+try:
+    open("config.txt")
+except OSError as err:
+     if err.errno == 2:
+         print("Couldn't find the config.txt file!")
+     elif err.errno == 13:
+        print("Found config.txt but couldn't read it")
