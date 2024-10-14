79975c904cc3e75512ad6f33756b3b4b7673b9c0
Jesus Duran <jesusduran141014@gmail.com>
2024-10-14 12:51:37 +0200

    agua astronautas

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-loaded_config = """# Rocket Ship Configuration File!
-fuel_tanks=4
-oxygen_tanks=3
-initial_propulsion_level=84
-$ End of file"""
+def alert_navigation_system(err):
+   print(err)
+def water_left(astronauts, water_left, days_left):
+    for argument in [astronauts, water_left, days_left]:
+        try:
+          argument / 10
+        except TypeError:
+           raise TypeError(f"All arguments must be of type int, but recieved: '{argument}'")
+          
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+       raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+try:
+ water_left(5,"100",2)
+except RuntimeError as err:
+   alert_navigation_system(err)
+except TypeError as err:
+   alert_navigation_system(err)
 
-parsed_config = {}
-for line in loaded_config.split('\n'):
-    try:
-        key, value = line.split('=')
-        parsed_config[key] = value
-    except ValueError:
-        print(f'Unable to parse {line}')
-print(parsed_config)
+
+
+
