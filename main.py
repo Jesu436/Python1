f612d5515e1b3228a14a352a158a57345cb7226d
Jesus Duran <jesusduran141014@gmail.com>
2024-10-14 13:43:01 +0200

    true or false

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-def alert_navigation_system(err):
-   print(err)
-def water_left(astronauts, water_left, days_left):
-    for argument in [astronauts, water_left, days_left]:
-        try:
-          argument / 10
-        except TypeError:
-           raise TypeError(f"All arguments must be of type int, but recieved: '{argument}'")
-          
-    daily_usage = astronauts * 11
-    total_usage = daily_usage * days_left
-    total_water_left = water_left - total_usage
-    if total_water_left < 0:
-       raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
-    return f"Total water left after {days_left} days is: {total_water_left} liters"
-try:
- water_left(5,"100",2)
-except RuntimeError as err:
-   alert_navigation_system(err)
-except TypeError as err:
-   alert_navigation_system(err)
+true_values = ['yes', 'y']
+false_values = ['no', 'n']
 
+def str_to_bool(value):
+    value = value.lower()
+    if value in true_values:
+        return True
+    elif value in false_values:
+        return False
+    else:
+        raise ValueError('invalid entry')
+    
+    
+try:
+    print(str_to_bool("n"))
+except Exception as ex:
+    print(ex)
+
+    
 
 
 
